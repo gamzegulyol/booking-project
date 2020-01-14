17b32967995cf72748bc75064bdcf832c4a3a8ec
gamzegulyol <ggulyol@gmail.com>
2020-01-14 21:11:04 +0300

    room added to the reservation entity

--- model/reservation.js
+++ model/reservation.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose'); 
+const Room = require('./room');
 let date_ob = new Date();
 module.exports = mongoose.model('Reservation',
     mongoose.Schema({
         id: String,
         createdAt : date_ob.getDate(),
         updatedAt : date_ob.getDate(),
+        room:[Room.schema],
         customerName:
         {
             type:String,
@@ -16,6 +18,7 @@ module.exports = mongoose.model('Reservation',
             required: true,
             unique: true
         }
+        
     }));
 
 module.exports = mongoose.model('Reservation', ReservationSchema);
